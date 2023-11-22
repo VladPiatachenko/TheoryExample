6367e5a0329998bdb56c1102e888377c14a0e526
Erlkonig <vl.piatachenko@cs.sumdu.edu.ua>
2023-11-22 22:40:09 +0200

    manual stage

---
+++ SpringExample/src/main/java/sumdu/edu/ua/springexample/App.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sumdu.edu.ua.springexample;
+
+/**
+ *
+ * @author Oksana
+ */
+public class App {
+    public static void main(String[] args){
+        Soda a=new Soda();
+        a.setName("Cola");
+        a.info();
+    }
+}
---
+++ SpringExample/src/main/java/sumdu/edu/ua/springexample/Soda.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sumdu.edu.ua.springexample;
+
+/**
+ *
+ * @author Oksana
+ */
+public class Soda {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void info(){
+        System.out.println("It's "+name);
+    }
+}
